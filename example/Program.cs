8237d22f7d0f4e5607ca47d1f47b1fc96732caf4
Ricardo Coelho <tiorac@msn.com>
2023-01-20 23:40:19 -0300

    Refactor and Support Magic Service

--- example/Program.cs
+++ example/Program.cs
@@ -1,5 +1,8 @@
+using BlackDigital.Blazor;
 using BlackDigital.Blazor.DataBuilder;
+using BlackDigital.Rest;
 using Example;
+using Example.Service;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,7 +10,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(sp => new RestClient("https://openlibrary.org"));
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => ComponentDataBuilder.New());
+
+builder.Services.AddRestService(restService => restService
+    .AddService<IOpenLibrary>()
+);
 
 await builder.Build().RunAsync();
---
+++ example/Service/IOpenLibrary.cs
@@ -0,0 +1,12 @@
+﻿using BlackDigital.Rest;
+using System.Text.Json;
+
+namespace Example.Service
+{
+    [Service("books")]
+    public interface IOpenLibrary
+    {
+        [Action("{key}.json")]
+        Task<Book> GetBook([FromRoute] string key);
+    }
+}
